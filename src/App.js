af73165d131b0076acecb2c9fb605fd107592671
UGUR BARAN DUNDAR <ubarandundar@gmail.com>
2023-02-28 13:19:42 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartIsShown(true)
+    setCartIsShown(true);
   };
 
   const hideCartHandler = () => {
-    setCartIsShown(false)
+    setCartIsShown(false);
   };
 
   return (
